9d08483e2b2357dc7ee00c878ffa90227e63ad26
Ha Thai Hoang <hoanght@bap.jp>
2021-04-07 16:50:50 +0700

    init

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/8.3.2/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/8.3.2/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/7.2.2/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/7.2.2/firebase-messaging.js');
 
 firebase.initializeApp({
   'messagingSenderId': '469986151760'
